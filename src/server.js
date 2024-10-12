6abaff963cd6abf5659674d564029ee6a73e0cdb
Yash Shukla <yshukla2775@gmail.com>
2024-10-13 02:12:13 +0530

    connect to mongodb atlas

---
+++ src/server.js
@@ -0,0 +1,38 @@
+
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+/*
+import express from 'express'
+
+const app = express()
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=> {
+            console.log(`app listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR : ", error)
+        throw error
+    }
+})()
+*/
