8081ec0c76cc2a67411a87114dac944a29940e4b
Yash Shukla <yshukla2775@gmail.com>
2025-03-22 14:44:08 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+      credentials: true,
+    })
+  );
 
 app.use(express.json({limit: "16kb"}))
 app.use(urlencoded({extended: true, limit: "16kb"}))
