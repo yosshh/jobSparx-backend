c2acdac5de250263c012e72a3e4361ac79768d40
Yash Shukla <yshukla2775@gmail.com>
2025-03-22 13:31:50 +0530

    changes made for deployment

--- src/server.js
+++ src/server.js
@@ -13,10 +13,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: [process.env.CORS_ORIGIN, 'http://localhost:5173'],
         credentials: true
     }
 });
+
 
 io.on("connection", (socket)=> {
     console.log("A user connected: "+ socket.id);
